0a5dcee0dbae1ad9b800dc4f424206f644bd4a18
Zephexia <dabaconator05@gmail.com>
2025-04-09 12:37:01 -0400

    Update Models.py

--- Models.py
+++ Models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#Stores the name of items for our database as well as the quantity
 class Item(models.Model):
     name = models.CharField(max_length=100)
     quantity = models.PositiveIntegerField()
 
+#A foreign key that links each inventory record to a specific item
 class Inventory(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
